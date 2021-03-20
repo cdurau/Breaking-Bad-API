945b8ec956bff17942eb5c11a1ba4d2e271ee5a1
Cedric <3326546+cdurau@users.noreply.github.com>
2021-03-20 17:54:31 +0100

    Added Search Component

--- src/components/characters/CharacterGrid.js
+++ src/components/characters/CharacterGrid.js
@@ -5,7 +5,7 @@ const CharacterGrid = ({ items, isLoading }) => {
     return isLoading ? (
         <h1>Loading...</h1>
     ) : (
-        <section className="cards">
+        <section className='cards'>
             {items.map(item => (
                 <CharacterItem key={item.char_id} item={item}></CharacterItem>
             ))}
