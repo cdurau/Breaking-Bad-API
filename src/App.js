48b8aa776029b6cfae2bdc41e17a842222df4571
Cedric <3326546+cdurau@users.noreply.github.com>
2021-03-20 17:07:36 +0100

    get data from API

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Header from './components/ui/Header';
+import './App.css';
+
+// https://youtu.be/YaioUnMw0mo?t=588
+
+const App = () => {
+    const [items, setItems] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchItems = async () => {
+            const result = await axios('https://www.breakingbadapi.com/api/characters');
+
+            setItems(result.data);
+            setIsLoading(false);
+        };
+
+        fetchItems();
+    }, []);
+
+    return (
+        <div className="container">
+            <Header />
+        </div>
+    );
+};
+
+export default App;
